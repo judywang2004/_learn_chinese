a589539fb5a460a1ab969873aa3fbce518990b73
judywang2004 <judywang2004@gmail.com>
2018-06-10 11:14:29 -0400

    singletontest

---
+++ SingletonTest/Y.java
@@ -0,0 +1,7 @@
+package SingletonTest;
+
+public class Y {
+	public Y(){System.out.println("Y"); doSomthing();}
+	public void doSomthing(){System.out.println("Y do somthing");}
+
+}
---
+++ SingletonTest/Z.java
@@ -0,0 +1,8 @@
+package SingletonTest;
+
+public class Z extends Y{
+	public Z(){
+		System.out.println("Z"); doSomthing();
+	}
+	public void doSomthing(){System.out.println("Z do somthing");}
+}
